0bd2a6575b0baa3af34e16e70ecc375bab8f7e95
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-09-29 23:29:09 +1000

    Integrate ScalableText example into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { MockContainer } from 'components/MockContainer'
 import { ScalableInput } from 'components/ScalableInput'
+import { ScalableText } from 'components/ScalableText'
 import React from 'react'
 
 const allowedCharacters = Array.from('abcdefghijklmnopqrstuvwxyz -_1234567890')
@@ -16,8 +17,14 @@ const handleSubit = (evt: React.FormEvent<HTMLFormElement>): void => {
   evt.preventDefault()
   const data = new FormData(evt.target as HTMLFormElement)
 
+  // eslint-disable-next-line no-console
   console.log(Object.fromEntries(data.entries()))
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const longText = (
+  'This is some very long text that should hopefully not wrap but instead scale to a smaller size'
+)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const mockupForm = (
@@ -35,19 +42,47 @@ const mockupForm = (
   </form>
 )
 
+const pStyle = { marginBottom: 4, paddingBottom: 2, borderBottom: '1px solid white' }
+
 function App(): React.ReactNode {
   return (
     <MockContainer
       fillHeight
       flex
     >
-      <div>
-        Hello, world!
+      <div
+        style={{
+          width: 170,
+          flexShrink: 0,
+        }}
+      >
+        <p style={pStyle}><strong>Multiline</strong></p>
+        <ScalableText
+          text={'There\'s a squirrellongername in my pants'}
+          wrap
+          // minFontSize={16}
+        />
+
+        <p style={pStyle}><strong>Shrink (short text)</strong></p>
+        <ScalableText
+          text={'Completed Levels: 0'}
+          minFontSize={16}
+        />
+
+        <p style={pStyle}><strong>Shrink (long text)</strong></p>
+        <ScalableText
+          text={'Completed Levels: 99997'}
+        />
+
+        <p style={pStyle}><strong>Shrink (min font size)</strong></p>
+        <ScalableText
+          text={'Completed Levels: 99997'}
+          minFontSize={20}
+        />
       </div>
       <div
         style={{
-          width: 600,
-          flexShrink: 0,
+          // width: 400,
         }}
       >
         Hello, world!
