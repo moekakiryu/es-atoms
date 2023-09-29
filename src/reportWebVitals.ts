c65d7ba62827e4e0032f66576331ba5a6adaeff4
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-09-29 18:31:33 +1000

    Fix linting errors

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,19 @@
-import { ReportHandler } from 'web-vitals';
+import type { ReportHandler } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry)
+        getFID(onPerfEntry)
+        getFCP(onPerfEntry)
+        getLCP(onPerfEntry)
+        getTTFB(onPerfEntry)
+      })
+      .catch(() => {
+        throw new Error('Failed loading web vitals')
+      })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
