512b6d745f1faa50ba27b6d10a9abd73da147eba
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-09-29 20:13:25 +1000

    Remove unused css file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './root.css'
+import 'shared/styles/main.scss'
 import App from './App'
 
 const root = ReactDOM.createRoot(
